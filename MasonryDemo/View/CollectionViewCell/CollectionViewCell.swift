101c1d9dd96f57dc5271d8afdb6176e302c7a3f4
alangprs <alangprs1@gmail.com>
2022-04-17 22:57:10 +0800

    新增 collectionView 基礎內容設定

---
+++ MasonryDemo/View/CollectionViewCell/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  MasonryDemo
+//
+//  Created by 翁燮羽 on 2022/4/17.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
--- MasonryDemo/ViewController.swift
+++ MasonryDemo/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        initUI()
     }
-
+    
+    func initUI() {
+        collectionViewSetUp()
+    }
 
 }
 
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
+    
+    func collectionViewSetUp() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        let nib = UINib(nibName: "\(CollectionViewCell.self)", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: "\(CollectionViewCell.self)")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(CollectionViewCell.self)", for: indexPath) as? CollectionViewCell else {
+            print("Viewcontroller Get cell fail")
+            return UICollectionViewCell()
+        }
+        
+        return cell
+    }
+    
+    
+}
